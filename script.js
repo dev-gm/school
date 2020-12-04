c12457d01adbfc3c293f8ab5d9273b5ef6d7ac1c
Gavin Mason <github@gavinm.us>
2020-12-04 15:10:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var filter, input, ul, li, a, i, title txtValue;
+
+function search() {
+    title = document.querySelector("title");
+    input = document.querySelector("#input").value;
+    if (input == "moneys") {
+      title.innerText = "$$$$$$$$$$$$";
+    }
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("ul");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
